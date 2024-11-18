0767251c20f146c2c92b502a8075a3109b44f390
irfanarga <irfanargaprasetya81@gmail.com>
2024-11-18 13:48:29 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const Hapi = require('@hapi/hapi');
+const NoteService = require('./services/inMemory/NotesService');
+const notes = require('./api/notes');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NoteService();
+  const server = Hapi.server({
+    port: 3000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
